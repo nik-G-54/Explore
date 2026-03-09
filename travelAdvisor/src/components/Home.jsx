bef5de8287dc749ecab60741e1a10cd8e7e91e81
Nikhil <nikhilgupta542006@gmail.com>
2026-03-09 23:06:13 +0530

    error resolve in the home section

--- travelAdvisor/src/components/Home.jsx
+++ travelAdvisor/src/components/Home.jsx
@@ -18,3 +18,23 @@ const Home = () => {
 }
 
 export default Home
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// const Home = () => {
+//      const navigate = useNavigate()
+//   return (
+//     <div>
+//       hi this is the home page 
+//      <button
+//               type="submit"
+//               className="btn-primary btn-light "
+//               onClick={() => navigate("/Login")}
+//             >
+//         Login
+//             </button>
+   
+//     </div>
+//   )
+// }
+
+// export default Home
